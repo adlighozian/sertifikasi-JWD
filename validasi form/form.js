c20ab86669d02b8efcdb6d2a468faf6a81d97019
adlighozian <adlighozian1@gmail.com>
2021-10-20 14:00:48 +0700

    validasi form

--- validasi form/form.js
+++ validasi form/form.js
@@ -1,30 +1,30 @@
 function cekForm() {
     const foto = document.getElementById('foto')
-    if (foto.value === '') {
+    if (foto.value === '' || foto.value === 'null') {
         isInvalid(foto, 'foto', 'Foto tidak boleh kosong')
     }
 
     const namaLengkap = document.getElementById('namalengkap')
-    if (namaLengkap.value === '') {
+    if (namaLengkap.value === '' || namaLengkap.value === 'null') {
         isInvalid(namaLengkap, 'namalengkap', 'Nama lengkap tidak boleh kosong')
     } else if (namaLengkap.value.length <= 3) {
         isInvalid(namaLengkap, 'namalengkap', 'Nama lengkap minimal 3 karakter')
     }
 
     const namaPanggilan = document.getElementById('namapanggilan')
-    if (namaPanggilan.value === '') {
+    if (namaPanggilan.value === '' || namaPanggilan.value === 'null') {
         isInvalid(namaPanggilan, 'namapanggilan', 'Nama panggilan tidak boleh kosong')
     } else if (namaPanggilan.value.length <= 3) {
         isInvalid(namaPanggilan, 'namapanggilan', 'Nama panggilan minimal 3 karakter')
     }
 
     const tempatLahir = document.getElementById('tempatlahir')
-    if (tempatLahir.value === '') {
+    if (tempatLahir.value === '' || tempatLahir.value === 'null') {
         isInvalid(tempatLahir, 'tempatlahir', 'Tempat lahir tidak boleh kosong')
     }
 
     const tanggalLahir = document.getElementById('tanggallahir')
-    if (tanggalLahir.value === '') {
+    if (tanggalLahir.value === '' || tanggalLahir.value === 'null') {
         isInvalid(tanggalLahir, 'tanggallahir', 'Tanggal lahir tidak boleh kosong')
     } else {
         const usia = document.getElementById('usia')
@@ -39,17 +39,17 @@ function cekForm() {
     }
 
     const kotaTinggal = document.getElementById('kotatinggal')
-    if (kotaTinggal.value === '') {
+    if (kotaTinggal.value === '' || kotaTinggal.value === 'null') {
         isInvalid(kotaTinggal, 'kotatinggal', 'Kota tinggal tidak boleh kosong')
     }
 
     const email = document.getElementById('email')
-    if (email.value === '') {
+    if (email.value === '' || email.value === 'null') {
         isInvalid(email, 'email', 'Email tidak boleh kosong')
     }
 
     const noHp = document.getElementById('nohp')
-    if (noHp.value === '') {
+    if (noHp.value === '' || noHp.value === 'null') {
         isInvalid(noHp, 'nohp', 'Nomor handphone tidak boleh kosong')
     } else if (no.value < 4) {
         isInvalid(noHp, 'nohp', 'Nomor handphone minimal 4 karakter')
