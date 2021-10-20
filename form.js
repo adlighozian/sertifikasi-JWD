237e7202ab7ecaf3df1901513d35df28b4cb7aae
adlighozian <adlighozian1@gmail.com>
2021-10-20 13:38:40 +0700

    membuat form

---
+++ form.js
@@ -0,0 +1,76 @@
+function cekForm() {
+    const foto = document.getElementById('foto')
+    if (foto.value === '') {
+        isInvalid(foto, 'foto', 'Foto tidak boleh kosong')
+    }
+
+    const namaLengkap = document.getElementById('namalengkap')
+    if (namaLengkap.value === '') {
+        isInvalid(namaLengkap, 'namalengkap', 'Nama lengkap tidak boleh kosong')
+    } else if (namaLengkap.value.length <= 3) {
+        isInvalid(namaLengkap, 'namalengkap', 'Nama lengkap minimal 3 karakter')
+    }
+
+    const namaPanggilan = document.getElementById('namapanggilan')
+    if (namaPanggilan.value === '') {
+        isInvalid(namaPanggilan, 'namapanggilan', 'Nama panggilan tidak boleh kosong')
+    } else if (namaPanggilan.value.length <= 3) {
+        isInvalid(namaPanggilan, 'namapanggilan', 'Nama panggilan minimal 3 karakter')
+    }
+
+    const tempatLahir = document.getElementById('tempatlahir')
+    if (tempatLahir.value === '') {
+        isInvalid(tempatLahir, 'tempatlahir', 'Tempat lahir tidak boleh kosong')
+    }
+
+    const tanggalLahir = document.getElementById('tanggallahir')
+    if (tanggalLahir.value === '') {
+        isInvalid(tanggalLahir, 'tanggallahir', 'Tanggal lahir tidak boleh kosong')
+    } else {
+        const usia = document.getElementById('usia')
+        let age = calculateAge(new Date(tanggalLahir.value))
+
+        usia.value = age
+    }
+
+    const jenisKelamin = document.getElementById('jeniskelamin')
+    if (jenisKelamin.value === '#') {
+        isInvalid(jenisKelamin, 'jeniskelamin', 'Jenis kelamin tidak boleh kosong')
+    }
+
+    const kotaTinggal = document.getElementById('kotatinggal')
+    if (kotaTinggal.value === '') {
+        isInvalid(kotaTinggal, 'kotatinggal', 'Kota tinggal tidak boleh kosong')
+    }
+
+    const email = document.getElementById('email')
+    if (email.value === '') {
+        isInvalid(email, 'email', 'Email tidak boleh kosong')
+    }
+
+    const noHp = document.getElementById('nohp')
+    if (noHp.value === '') {
+        isInvalid(noHp, 'nohp', 'Nomor handphone tidak boleh kosong')
+    } else if (no.value < 4) {
+        isInvalid(noHp, 'nohp', 'Nomor handphone minimal 4 karakter')
+    }
+
+    const mediaSosial = document.getElementById('mediasosial')
+    if (mediaSosial.value === '#') {
+        isInvalid(mediaSosial, 'mediasosial', 'Media sosial tidak boleh kosong')
+    }
+
+    event.preventDefault();
+}
+
+function isInvalid(selector, id, message) {
+    selector.classList.add('is-invalid')
+    document.getElementById(id + '_validation').innerHTML = message
+}
+
+function calculateAge(date) {
+    const now = new Date();
+    const diff = Math.abs(now - date);
+    const age = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
+    return age
+}
